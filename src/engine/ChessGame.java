28ef1103238fb4c670dea2dce6390a76d36b4748
Camille Theubet <camille.theubet@hes-so.ch>
2024-11-28 14:23:15 +0100

    Piece are at starting pos

--- src/engine/ChessGame.java
+++ src/engine/ChessGame.java
@@ -24,6 +24,24 @@ public class ChessGame implements ChessController {
   @Override
   public void newGame() {
     view.displayMessage("new game (TO REMOVE)"); // TODO
-    view.putPiece(PieceType.KING, PlayerColor.BLACK, 3, 4); // TODO exemple uniquement
+    // view.putPiece(PieceType.KING, PlayerColor.BLACK, 3, 4); // TODO exemple uniquement
+    int pieceStartRow;
+    int pawnStartRow;
+    for (PlayerColor color : PlayerColor.values()) {
+      pieceStartRow = color.ordinal() == PlayerColor.WHITE.ordinal() ? 0 : 7;
+      pawnStartRow = color.ordinal() == PlayerColor.WHITE.ordinal() ? 1 : 6;
+      view.putPiece(PieceType.ROOK, color, 0, pieceStartRow);
+      view.putPiece(PieceType.ROOK, color, 7, pieceStartRow);
+      view.putPiece(PieceType.KNIGHT, color, 1, pieceStartRow);
+      view.putPiece(PieceType.KNIGHT, color, 6, pieceStartRow);
+      view.putPiece(PieceType.BISHOP, color, 2, pieceStartRow);
+      view.putPiece(PieceType.BISHOP, color, 5, pieceStartRow);
+      view.putPiece(PieceType.QUEEN, color, 3, pieceStartRow);
+      view.putPiece(PieceType.KING, color, 4, pieceStartRow);
+
+      for (int i = 0 ; i < 8 ; ++i){
+        view.putPiece(PieceType.PAWN, color, i, pawnStartRow);
+      }
+    }
   }
 }
