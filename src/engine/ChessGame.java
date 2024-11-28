c641cbc25efeeddfce1132840bdfcc3c5b118fa5
Schouwey <zazar981@hotmail.com>
2024-11-28 16:02:53 +0100

    boardtoview

--- src/engine/ChessGame.java
+++ src/engine/ChessGame.java
@@ -4,13 +4,17 @@ import chess.ChessController;
 import chess.ChessView;
 import chess.PieceType;
 import chess.PlayerColor;
+import engine.piece.Piece;
 
 public class ChessGame implements ChessController {
 
   private ChessView view;
 
+  private Piece[][] board;
+
   @Override
   public void start(ChessView view) {
+    this.board = new Piece[8][8];
     this.view = view;
     view.startView();
   }
@@ -41,6 +45,25 @@ public class ChessGame implements ChessController {
 
       for (int i = 0 ; i < 8 ; ++i){
         view.putPiece(PieceType.PAWN, color, i, pawnStartRow);
+      }
+    }
+  }
+
+  public void boardToView(){
+    for(int i = 0 ; i < 8 ; ++i){
+      for(int j = 0 ; j < 8 ; ++j){
+        if(board[i][j] != null){
+          PieceType p = switch (board[i][j].getClass().getSimpleName()){
+            case "King" -> PieceType.KING;
+            case "Bishop" -> PieceType.BISHOP;
+            case "Queen" -> PieceType.QUEEN;
+            case "Knight" -> PieceType.KNIGHT;
+            case "Pawn" -> PieceType.PAWN;
+            default -> PieceType.BISHOP;
+          };
+          view.putPiece(p,PlayerColor.BLACK,i,j);
+
+        }
       }
     }
   }
