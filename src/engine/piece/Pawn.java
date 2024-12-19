eaea1976a144ffdf7bf6bd380518b13c92425ace
Chomusuke <yukhanov.artur@gmail.com>
2024-12-19 12:24:54 +0100

    Implemented capture for pawns

--- src/engine/piece/Pawn.java
+++ src/engine/piece/Pawn.java
@@ -6,6 +6,8 @@ import engine.Coordinates;
 import engine.movements.*;
 
 public class Pawn extends Piece {
+	private final Movement[] captureRestrictions;
+
 	public Pawn (PlayerColor color, Coordinates<Integer> coordinates) {
 		super(color, coordinates,
 			new Movement[] {
@@ -16,7 +18,23 @@ public class Pawn extends Piece {
 				new RadiusMovementRestriction(2)  // Radius changed to 1 after first move
 			}
 		);
+
+		captureRestrictions = new Movement[] {
+			new DirectionMovementRestriction(color),
+			new RadiusMovementRestriction(1),
+			new DiagonalMovement()
+		};
 	}
+
+	@Override
+	public boolean canCaptureAt(Coordinates<Integer> at) {
+		for (var movement : captureRestrictions) {
+			if (!movement.canMove(getCoordinates(), at)) return false;
+		}
+
+		return true;
+	}
+
 	@Override
 	public PieceType getGraphicalType() {
 		return PieceType.PAWN;
