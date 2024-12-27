d7d93376cc55e3442462a7f5a8d41b2c769f6747
Chomusuke <yukhanov.artur@gmail.com>
2024-12-27 13:53:35 +0100

    Implemented method to shift a coordinate

--- src/engine/Coordinates.java
+++ src/engine/Coordinates.java
@@ -1,3 +1,11 @@
 package engine;
 
-public record Coordinates<T extends Number>(T x, T y) {}
+public record Coordinates<T extends Number>(T x, T y) {
+	// TODO: remove generic
+	public Coordinates<Integer> move(T dx, T dy) {
+		int rx = this.x().intValue() + dx.intValue();
+		int ry = this.y().intValue() + dy.intValue();
+
+		return new Coordinates<>(rx, ry);
+	}
+}
