1d8c4ebfadb00e8f75fb8b49193c897f3900e9ea
Chomusuke <yukhanov.artur@gmail.com>
2024-12-27 13:53:57 +0100

    Implemented base for castle mechanic

--- src/engine/piece/King.java
+++ src/engine/piece/King.java
@@ -1,13 +1,14 @@
 package engine.piece;
 
 import chess.PieceType;
-import chess.PieceType;
 import chess.PlayerColor;
 import engine.Coordinates;
 import engine.movements.*;
 
 public class King extends Piece {
-	private boolean hasMoved;
+	private static final int CASTLE_DIST = 2;
+	private boolean canCastle = true;
+
 	public King (PlayerColor color, Coordinates<Integer> coordinates){
 		super(color, coordinates,
 			new Movement[] {
@@ -19,8 +20,26 @@ public class King extends Piece {
 			}
 		);
 	}
+
+	@Override
+	public void moveTo(Coordinates<Integer> coordinates) {
+		super.moveTo(coordinates);
+
+		canCastle = false;
+	}
+
     @Override
     public PieceType getGraphicalType() {
         return PieceType.KING;
     }
+
+	public void castle(Rook rook) {
+		if (!canCastle) return;
+
+		int direction = (rook.getCoordinates().x() < this.getCoordinates().x() ? -1 : 1);
+		int movement = direction * CASTLE_DIST;
+
+		moveTo(this.coordinates.move(movement, 0));
+		rook.moveTo(this.coordinates.move(-direction, 0));
+	}
 }
